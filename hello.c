08b1c100292b91aa10c4252b07b0ac8455558d39
Mario Simental <simentalma@yahoo.com>
2024-01-25 12:00:39 -0600

    replaced with sample C program

--- hello.c
+++ hello.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
-int main()
-{
-printf("%s\n", "bye world");
+#include <string.h>
+int main() {
+char str[100];
+int i, len;
+printf("Enter a string: ");
+gets(str);
+len = strlen(str);
+for (i = 0; i < len; i++) {
+if (str[i] >= 'a' && str[i] <= 'z') {
+str[i] = str[i] - 'a' + 'A';
+}
+}
+printf("The capitalized string is: %s\n", str);
+// Reverse the string.
+for (i = 0, len = strlen(str); i < len / 2; i++) {
+char temp = str[i];
+str[i] = str[len - 1 - i];
+str[len - 1 - i] = temp;
+}
+printf("The reversed string is: %s\n", str);
 return 0;
 }
